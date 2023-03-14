1df462b3749916d34a2528fd8e8f765543e58c1b
Cecilia Daninthe <cecilia.daninthe@gmail.com>
2023-03-14 18:34:12 -0400

    commit tree dd45cc45c0e15010b97d8bbe23096512b311a000

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+class Anagram
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match (arr)
+        arr.filter do |word|
+            word.chars.sort == @word.chars.sort
+        end
+    
+    end
+end
